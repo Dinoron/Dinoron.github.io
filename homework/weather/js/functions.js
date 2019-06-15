27e5b77730a1f99bfa02d900af71516998980b20
Dinoron <RonnieBoonthavee@gmail.com>
2019-06-15 01:12:52 -0600

    added js

---
+++ homework/weather/js/functions.js
@@ -0,0 +1,7 @@
+/* *************************************
+*  Weather Site JavaScript Functions
+************************************* */
+
+// Weather Site JavaScript Functions
+
+console.log('My javascript is being read.');
