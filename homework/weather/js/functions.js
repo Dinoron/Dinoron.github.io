0432c10d3a5a08cb1853e8e52bb9dbf0dfac8c48
Dinoron <RonnieBoonthavee@gmail.com>
2019-06-23 17:09:30 -0600

    l

--- homework/weather/js/functions.js
+++ homework/weather/js/functions.js
@@ -85,5 +85,6 @@ function windDial(direction){
     }
    }
    
+   
 
    
---
+++ homework/weather/js/inclass.js
@@ -0,0 +1,9 @@
+var string1 = "Hello";
+var string2 = "World";
+add(string1,string2);
+
+function add(string1, string2) {
+    var newString= string1 + " " + string2;
+    console.log(newString);
+    return newString 
+}
