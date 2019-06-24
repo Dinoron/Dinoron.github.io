2455a80239b8257d5acab9cc1b53b3a5b069914b
Dinoron <RonnieBoonthavee@gmail.com>
2019-06-24 00:45:01 -0600

    added fetch

--- homework/weather/js/localdata.js
+++ homework/weather/js/localdata.js
@@ -84,3 +84,17 @@ function fetchData(weatherURL){
   statusContainer.innerHTML = 'Sorry, the data could not be processed.';
   })
 }
+
+fetch(URL)
+ .then(function(response) {
+   if(response.ok){
+     return response.json();
+   }
+   throw new ERROR('Network response was not OK.');
+ })
+ .then(function(data){
+   ... do something with the JavaScript object ...
+ })
+ .catch(function(error){
+console.log('There was a fetch problem: ', error.message);
+ })
